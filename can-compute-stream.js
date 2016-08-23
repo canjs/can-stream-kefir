5aaa6df8c792d0e247cfc2fb1df169408dd05937
Chris Gomez <akagomez@gmail.com>
2016-08-23 10:07:48 -0500

    First draft of API and tests

---
+++ can-compute-stream.js
@@ -0,0 +1,63 @@
+import compute from 'can-compute';
+import Kefir from 'kefir';
+
+function makeArray(arr) {
+	var ret = [];
+	for (var i = 0; i < arr.length; i++) {
+		ret[i] = arr[i];
+	}
+	return ret;
+}
+
+var singleComputeToStream = function (compute) {
+	return Kefir.stream(function (emitter) {
+		var changeHandler = function (ev, newVal) {
+			emitter.emit(newVal);
+		};
+
+		emitter.emit(compute());
+		compute.on('change', changeHandler);
+
+		return function () {
+			compute.off('change', changeHandler);
+		}
+	});
+}
+
+var computeStream = function () {
+	var computes = makeArray(arguments);
+	var evaluator;
+
+	if (computes[computes.length - 1].hasOwnProperty('isComputed')) {
+		evaluator = function () {
+			return arguments.length > 1
+				? Kefir.merge(arguments)
+				: arguments[0];
+		}
+	} else {
+		evaluator = computes.pop();
+	}
+
+	var streams = computes.map(singleComputeToStream)
+
+	return evaluator.apply(undefined, streams);
+}
+
+computeStream.asCompute = function () {
+	var valueStream = computeStream.apply(undefined, arguments);
+	var computeUpdater;
+
+	var valueCompute = compute(undefined, {
+		on: function () {
+			valueStream.onValue(valueCompute)
+		},
+		off: function () {
+			valueStream.offValue(valueCompute)
+		}
+	});
+
+	return valueCompute;
+}
+
+
+export default computeStream;
--- can-compute-stream_test.js
+++ can-compute-stream_test.js
@@ -1,11 +1,71 @@
-var compute = require('can-compute');
-var QUnit = require('steal-qunit');
+import computeStream from 'can-compute-stream';
+import compute from 'can-compute';
+import QUnit from 'steal-qunit';
 
-QUnit.module('can/compute');
+QUnit.module('can-compute-stream');
 
-test('Hello world!', function () {
-	var foo = compute(0);
-	QUnit.equal(foo(), 0);
-	foo(1);
-	QUnit.equal(foo(), 1);
-});
+test('Compute changes can be streamed', function () {
+	var c = compute(0);
+	var stream = computeStream(c);
+	var computeVal;
+
+	stream.onValue(function (newVal) {
+		computeVal = newVal;
+	});
+
+	QUnit.equal(computeVal, 0);
+	c(1);
+
+	QUnit.equal(computeVal, 1);
+	c(2);
+
+	QUnit.equal(computeVal, 2);
+	c(3);
+
+	QUnit.equal(computeVal, 3);
+})
+
+test('Compute streams do not bind to the compute unless activated', function () {
+	var c = compute(0);
+	var stream = computeStream(c);
+
+	QUnit.equal(c.computeInstance._bindings, undefined);
+
+	stream.onValue(function () {});
+
+	QUnit.equal(c.computeInstance._bindings, 1);
+})
+
+test('Dependent compute streams do not bind to parent computes unless activated', function () {
+	var c1 = compute(0);
+	var c2 = compute(0);
+
+	var resultCompute = computeStream(c1, c2, function (s1, s2) {
+		return s1.merge(s2);
+	});
+
+	QUnit.equal(c1._bindings, undefined);
+	QUnit.equal(c2._bindings, undefined);
+})
+
+test('Compute stream values can be piped into a compute', function () {
+	var c1 = compute(0);
+	var c2 = compute(0);
+
+	var resultCompute = computeStream.asCompute(c1, c2, function (s1, s2) {
+		return s1.merge(s2);
+	})
+
+	resultCompute.on('change', function () {});
+
+	QUnit.equal(resultCompute(), 0)
+
+	c1(1)
+	QUnit.equal(resultCompute(), 1)
+
+	c2(2)
+	QUnit.equal(resultCompute(), 2)
+
+	c1(3);
+	QUnit.equal(resultCompute(), 3)
+})
