5ef920e7cfecdbb17047001489d337bb3b7e1494
Ivan <ivan.herndon@gmail.com>
2016-10-03 12:04:32 -0400

    placing define specific stream code into a separate repo

--- can-compute-stream.js
+++ can-compute-stream.js
@@ -1,10 +1,9 @@
 var compute = require('can-compute');
-var define = require('can-define');
 var Kefir = require('kefir');
 var makeArray = require("can-util/js/make-array/make-array");
-var isArray = require("can-util/js/is-array/is-array");
-var assign = require("can-util/js/assign/assign");
 
+
+// Pipes the value of a compute into a stream
 var singleComputeToStream = function (compute) {
 	return Kefir.stream(function (emitter) {
 		var changeHandler = function (ev, newVal) {
@@ -20,68 +19,103 @@ var singleComputeToStream = function (compute) {
 	});
 };
 
+// Converts all arguments passed to streams, and resolves the resulting
+// streams to a single stream
+//   Assumes all arguments are computes
+//   Last argument is optionally a function
 var computeStream = function () {
 	var computes = makeArray(arguments);
 	var evaluator;
 
+	// If the last argument is a compute, merge the streams by default
+	// Assume if last argument is a compute all arguments are computes
 	if (computes[computes.length - 1].hasOwnProperty('isComputed')) {
+
+		// Function will  be passed all resulting streams
 		evaluator = function () {
+
+			// If many streams are passed, merge them; Otherwise return the solo stream
 			return arguments.length > 1 ? Kefir.merge(arguments) : arguments[0];
 		};
+
+	// Assume the last argument is a method that will resolve all the 
+	// passed stream into a single stream
 	} else {
 		evaluator = computes.pop();
 	}
 
+	// Converts each individual compute to a stream
 	var streams = computes.map(singleComputeToStream);
 
+	// Resolves all of the streams to a single stream
 	return evaluator.apply(this, streams);
 };
 
+// A variation of `computeStream`, but pipes the values of the 
+// resolved stream back into a compute
 computeStream.asCompute = function () {
+
+	// Resolve a bunch of computes into a stream
 	var valueStream = computeStream.apply(this, arguments);
 	var streamHandler, lastValue;
 
+	// Create a compute that will bind to the resolved stream when bound
 	var valueCompute = compute(undefined, {
+
+		// When the compute is read, use that last value
 		get: function () {
 			return lastValue;
 		},
 		set: function (val) {
 			return val;
 		},
+
+		// When the compute is bound, bind to the resolved stream
 		on: function (updated) {
+
+			// When the stream passes a new values, save a reference to it and call
+			// the compute's internal `updated` method (which ultimately calls `get`)
 			streamHandler = function (val) {
 				lastValue = val;
 				updated();
 			};
 			valueStream.onValue(streamHandler);
 		},
+
+		// When the compute is unbound, unbind from the resolved stream
 		off: function () {
 			valueStream.offValue(streamHandler);
 		}
 	});
 
+	// Return the compute that's bound to the stream
 	return valueCompute;
 };
 
-define.behaviors.push('stream');
+	// computeStream.eventAsStream = function (map, property, eventName) {
+	// 	var lastValue, eventHandler;
 
-var oldExtensions = define.extensions;
+	// 	return compute(undefined, {
+	// 		// When the compute is read, use that last value
+	// 		get: function () {
+	// 			return lastValue;
+	// 		},
+	// 		set: function (val) {
+	// 			return val;
+	// 		},
 
-define.extensions = function (objPrototype, prop, definition) {
-	if (isArray(definition.stream)) {
-		return assign({
-			value: function () {
-				var map = this;
-				var computes = definition.stream
-					.map(function (arg) {
-						return typeof arg === 'string' ? compute(map, arg) : arg;
-					});
-				return computeStream.asCompute.apply(this, computes);
-			}
-		}, define.types.compute);
-	} else {
-		return oldExtensions.apply(this, arguments);
-	}
-};
+	// 		on: function (updated) {
+	// 			eventHandler = function (val) {
+	// 				lastValue = val;
+	// 				updated();
+	// 			};
+	// 			map.bind(eventName, eventHandler)
+	// 		},
+	// 		off: function () {
+	// 			map.unbind((eventName, eventHandler);
+	// 		}
+	// 	})
+	// }
+
 
 module.exports = computeStream;
--- can-compute-stream_test.js
+++ can-compute-stream_test.js
@@ -1,7 +1,6 @@
 var QUnit = require('steal-qunit');
 var computeStream = require('can-compute-stream');
 var compute = require('can-compute');
-var define = require('can-define');
 
 QUnit.module('can-compute-stream');
 
@@ -92,96 +91,4 @@ test('Computed streams fire change events', function () {
 
 	expected = 3;
 	c1(expected);
-});
-
-test('Stream map values into others', function () {
-
-	var allFooValue;
-	var MyMap = define.Constructor({
-		foo1: 'number',
-		foo2: 'number',
-		allFoo: {
-			type: 'compute',
-			value: function () {
-				var foo1 = compute(this, 'foo1');
-				var foo2 = compute(this, 'foo2');
-				return computeStream.asCompute(foo1, foo2);
-			}
-		}
-	});
-
-	var map = new MyMap();
-
-	map.on('allFoo', function (ev, newVal) {
-		allFooValue = newVal;
-	});
-
-	map.foo1 = 1;
-	QUnit.equal(allFooValue, 1);
-
-	map.foo2 = 2;
-	QUnit.equal(allFooValue, 2);
-
-	map.foo1 = 3;
-	QUnit.equal(allFooValue, 3);
-});
-
-
-test('Stream map values into others using sugar syntax', function () {
-	var expected;
-	var MyMap = define.Constructor({
-		foo1: 'number',
-		foo2: 'number',
-		allFoo: {
-			stream: ['foo1', 'foo2']
-		}
-	});
-
-	var map = new MyMap();
-
-	map.on('allFoo', function (ev, newVal) {
-		expected = newVal;
-	});
-
-	map.foo1 = 1;
-	QUnit.equal(expected, 1);
-
-	map.foo2 = 2;
-	QUnit.equal(expected, 2);
-
-	map.foo1 = 3;
-	QUnit.equal(expected, 3);
-});
-
-test('Stream into streams', function () {
-	var expected = 1;
-	var MyMap = define.Constructor({
-		foo1: {
-			value: 0
-		},
-		stream1: {
-			stream: ['foo1']
-		},
-		stream2: {
-			stream: ['stream1']
-		}
-	});
-
-	var map = new MyMap();
-
-	map.on('stream1', function (ev, newVal) {
-		QUnit.equal(newVal, expected, 'Stream 1 updated');
-	});
-
-	map.on('stream2', function (ev, newVal) {
-		QUnit.equal(newVal, expected, 'Stream 2 updated');
-	});
-
-	map.foo1 = expected;
-
-	expected = 2;
-	map.stream1 = expected;
-
-	expected = 3;
-	map.stream2 = expected;
 });
